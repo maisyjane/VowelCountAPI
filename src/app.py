3c0c9126289c575ca9125f4aa22907d5dab7e8c0
40225143 <msinclair06@qub.ac.uk>
2021-11-24 13:26:53 +0000

    added error handling and dockerfile

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 from flask import Response
+import json
 
 from vowel_counter import count_vowels
 
@@ -8,10 +9,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def vowelcount(): #needs error handling
-    string = str(request.args.get('x'))
-    num_of_vowels = str(count_vowels(string))
-    return num_of_vowels
+    if request.args.get('x'):
+        string = str(request.args.get('x'))
+        num_of_vowels = str(count_vowels(string))
+        
+        r = {
+            "String" : string,
+            "Vowels" : num_of_vowels
+        }
+    
+    else:
+        r = {
+            "String" : "No String entered",
+            "Vowels" : "N/A"
+        }
+
+    
+    reply = json.dumps(r)
+
+    response = Response(response = reply, status = 200, mimetype="application/json")
+
+    response.headers['Content-Type']='application/json'
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    
+
+
+    return response
 
 
 if __name__ == '__main__':
-    app.run(port = 5000)
+    app.run(host='0.0.0.0', port = 5000)
